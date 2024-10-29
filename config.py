43294835788d1265db1bfa7f4b9d83c0aebc9fea
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-10-30 03:47:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(getenv("OWNER_ID", 1356469075))
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", "HRKU-0936de05-e153-49ca-acd3-0b8a2649713a")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
